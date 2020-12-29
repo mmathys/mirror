5d8243a9d0ac340bc4d2024a6ac44c3320938634
Max Mathys <threadox@gmail.com>
2020-12-29 21:18:42 +0100

    launch script fix: using --app

--- launch-mirror.sh
+++ launch-mirror.sh
@@ -4,4 +4,4 @@
 @export DISPLAY=:0
 @mkdir -p /etc/chromium-browser/customizations
 @echo 'CHROMIUM_FLAGS="${CHROMIUM_FLAGS} --check-for-update-interval=31536000"' > /etc/chromium-browser/customizations/01-disable-update-check
-@chromium-browser --kiosk --force-device-scale-factor=2 --disable-cache --disable-restore-session-state --disable-component-update --noerrdialogs https://mirror-1598972059056.web.app
+@chromium-browser --kiosk --force-device-scale-factor=2 --disable-cache --disable-restore-session-state --disable-component-update --noerrdialogs --app=https://mirror-1598972059056.web.app
